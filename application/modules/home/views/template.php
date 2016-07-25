1b5c30329697ddfd064d7ebfab89db45f6868482
Wild Code <harissudrajat.23@gmail.com>
2016-07-25 23:39:08 +0700

    Update
    Remove application\modules\home\index - Copy.php
    Update title \\home\views\template.php

--- application/modules/home/views/template.php
+++ application/modules/home/views/template.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Gentellela Alela! | </title>
+    <title>Kantong Kehamilan HLMC</title>
 
 
     <link href="<?php echo base_url() ?>assets/plugins/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
